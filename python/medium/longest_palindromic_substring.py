ba96f357d3eabd34411b8b8b6b39db1be3ae58b5
AleksSH97 <69675875+AleksSH97@users.noreply.github.com>
2025-01-27 19:56:13 +0300

    [f] python (medium): update longest palindromic substring with better solution (not my solution) [+] python (medium): add argparser for two solutions: O(n3) and O(n2)

--- python/medium/longest_palindromic_substring.py
+++ python/medium/longest_palindromic_substring.py
@@ -1,13 +1,14 @@
 import shlex
 import sys
 import time
+import argparse
 
-def is_palindrome(s: str) -> bool:
-    reverse = s[::-1]
-    return s == reverse
+class Solution_slow:
+    def longestPalindrome_slow(s: str) -> str:
+        def is_palindrome(s: str) -> bool:
+            reverse = s[::-1]
+            return s == reverse
 
-class Solution:
-    def longestPalindrome(s: str) -> str:
         if len(s) == 1 or is_palindrome(s) or len(s) == 0:
             return s
 
@@ -18,12 +19,11 @@ class Solution:
                 return s[:1]
 
         substr_longest_len = 1
-        substr_len = 0
-        symbol_num = 0
+        substr_len = 1
         longest_substring = s[0:1]
 
         for i in range(0, len(s) + 1):
-            for j in range(i + 1, len(s) + 1):
+            for j in range(i + 2, len(s) + 1):
                 if is_palindrome(s[i:j]):
                     print(f'String: {s[i:j]} i{i} j{j} is palindrome')
                     substr_len = len(s[i:j])
@@ -32,19 +32,62 @@ class Solution:
 
                     substr_longest_len = len(s[i:j])
                     print(f'Got new substring length {substr_longest_len}')
-                    symbol_num = i
 
         return longest_substring
-
-if __name__ == '__main__':
-    start_time = time.time()
     
-    cmdline = " ".join(map(shlex.quote, sys.argv[1:]))
 
-    result = Solution.longestPalindrome(cmdline)
+class Solution:
+    def longestPalindrome(s: str) -> str:
+        n = len(s)
+
+        def expand_around_center(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            
+            return right - left - 1
+
+        start = 0
+        end = 0
+        for i in range(n):
+            odd = expand_around_center(i, i)
+            even = expand_around_center(i, i + 1)
+
+            max_len = max(odd, even)
+            if end - start < max_len:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start: end + 1]
+
+def main():
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(
+        prog='Longest Palindromic Substring problem: https://leetcode.com/problems/longest-palindromic-substring',
+        description='Two problem solutions: O(n³) and O(n)',
+    )
+
+    parser.add_argument('--complexity', type=str, required=True, choices=['O(n2)', 'O(n3)'],
+                        help='Time complexity of the solution: O(n2) or O(n3)')
+    parser.add_argument('--string', type=str, required=True,
+                        help='Input string to find the longest palindromic substring')
+    
+    args = parser.parse_args()
+
+    input_string = args.string
+
+    if args.complexity == 'O(n2)':
+        result = Solution.longestPalindrome(input_string)
+    elif args.complexity == 'O(n3)':
+        result = Solution_slow.longestPalindrome_slow(input_string)
+
     if (result):
         print(f'Longest palindromic substring: {result}\n\r')
     else:
         print("There is no palindromic string!\n\r")
 
     print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
