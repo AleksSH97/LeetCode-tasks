7b43c542b3ab84a4ddfef62c9642decb80f84485
AleksSH97 <ashabalin@nao-team.com>
2025-02-20 11:15:35 +0300

    [+] balanced_binary_tree: started making program

--- python/easy/balanced_binary_tree.py
+++ python/easy/balanced_binary_tree.py
@@ -1,25 +1,54 @@
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
+
+    def insert(self, val):
+        if self.val:
+            if val < self.val:
+                if self.left is None:
+                    self.left = TreeNode(val)
+                else:
+                    self.left.insert(val)
+            elif val > self.val:
+                if self.right is None:
+                    self.right = TreeNode(val)
+                else:
+                    self.right.insert(val)
+        else:
+            self.val = val
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.val),
+        if self.right:
+            self.right.PrintTree()
 
 
-class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-
+# class Solution:
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
 
 def main():
-    root = [3,9,20,None,None,15,7]
-    res = isBalanced(root)
+    root = TreeNode(3)
+    root.insert(9)
+    root.insert(20)
+    root.insert(None)
+    root.insert(None)
+    root.insert(15)
+    root.insert(7)
+    root.PrintTree()
+    # res = isBalanced(root)
 
-    if(res):
-        print('Height Balanced')
-    else:
-        print('NOT Height Balanced')
+    # if(res):
+    #     print('Height Balanced')
+    # else:
+    #     print('NOT Height Balanced')
 
 
 if __name__ == "__main__":
