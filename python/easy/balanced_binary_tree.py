b8d63e08160244de0b39e5e28a927dd03351cd4a
AleksSH97 <ashabalin@nao-team.com>
2025-02-26 23:44:19 +0300

    [+] balanced_binary_tree: add parsing binary tree from terminal [f] balanced_binary_tree: fix adding values to binary tree

--- python/easy/balanced_binary_tree.py
+++ python/easy/balanced_binary_tree.py
@@ -1,4 +1,19 @@
-from collections import deque
+import argparse
+import re
+
+def parse_binary_tree(input: str):
+    matches = re.findall(r'\d+|None', input)
+
+    numbers = []
+    for match in matches:
+        if match == "None":
+            numbers.append(None)
+        else:
+            numbers.append(int(match))
+
+    print(f'Tree nums: {numbers}')
+    return numbers
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,29 +23,27 @@ class TreeNode:
         self.right = None
 
     def insert(self, val):
-        if self.val:
-            if self.left is None:
-                print(f'Left tree: {val}')
-                self.left = TreeNode(val)
-            elif self.right is None:
-                print(f'Right tree: {val}')
-                self.right = TreeNode(val)
+        queue = [self]  # Start from root
+        while queue:
+            node = queue.pop(0)
+            if node.left is None:
+                node.left = TreeNode(val)
+                return
             else:
-                if self.left.left is None or self.left.right is None:
-                    print(f'Next left tree')
-                    self.left.insert(val)
-                else:
-                    print('Next right tree')
-                    self.right.insert(val)
-        else:
-            self.val = val
+                queue.append(node.left)
 
-    def PrintTree(self):
+            if node.right is None:
+                node.right = TreeNode(val)
+                return
+            else:
+                queue.append(node.right)
+
+    def print_tree(self):
         if self.left:
-            self.left.PrintTree()
+            self.left.print_tree()
         print(self.val),
         if self.right:
-            self.right.PrintTree()
+            self.right.print_tree()
 
     def display_tree(self, tree, level=0, prefix="Root: "):
         """(Made with DeepSeek) Recursively display the binary tree."""
@@ -40,22 +53,38 @@ class TreeNode:
                 self.display_tree(tree.left, level + 1, "L--- ")
                 self.display_tree(tree.right, level + 1, "R--- ")
 
+    def height_tree(self, tree, height=0):
+        if tree is not None:
+            if tree.left is not None or tree.right is not None:
+                self.height_tree(tree.left, height + 1)
+                self.height_tree(tree.right, height + 1)
+            else:
+                print(f'Height: {height}')
+
 
 # class Solution:
     # def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
 
 def main():
-    root = TreeNode(3)
-    print(f'Init TreeNode root with value: {root.val}')
-    root.insert(9)
-    root.insert(20)
-    root.insert(None)
-    root.insert(None)
-    root.insert(15)
-    root.insert(7)
-    root.PrintTree()
+    parser = argparse.ArgumentParser(prog='Balanced Binary Tree problem: https://leetcode.com/problems/balanced-binary-tree')
+    parser.add_argument('--tree', type=str, required=True,
+                        help='Input string to find is Binary Tree balanced or not (e.g. [3, 9, 20, None, None, 15, 7])')
+
+    args = parser.parse_args()
+    binary_tree = parse_binary_tree(args.tree)
+
+    # Start root of binary tree
+    root = TreeNode(binary_tree[0])
+
+    # Fill binary tree with values from parser
+    for i in range(1, len(binary_tree), 1):
+        root.insert(binary_tree[i])
+
+    root.print_tree()
     root.display_tree(root)
+    root.height_tree(root)
+
     # res = isBalanced(root)
 
     # if(res):
