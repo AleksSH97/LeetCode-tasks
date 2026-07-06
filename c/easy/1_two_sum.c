aa416744a40a5feaf7ede2ba2b2bad792790aa2f
AleksSH97 <alexnv97@gmail.com>
2026-07-06 22:47:29 +0300

    [+] add hashMap solution for Two Sum problem

--- c/easy/1_two_sum.c
+++ c/easy/1_two_sum.c
@@ -1,3 +1,57 @@
+#include "../../libs/uthash/src/uthash.h"
+
+#define SOLUTION_BRUTE_FORCE (1)
+#define SOLUTION_HASH_TABLE  (2)
+
+#define SOLUTION SOLUTION_HASH_TABLE
+
+#if SOLUTION == SOLUTION_HASH_TABLE
+// O(n) - time, O(n) - memory
+struct hashTable {
+    int key;
+    int value;
+    UT_hash_handle hh;
+} *hashTable = NULL, *item, *tmpItem;
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        // Finding item that has key = nums[i]
+        HASH_FIND_INT(hashTable, &nums[i], item);
+        if (item != NULL) {
+            // Matching item was found, allocating memory for resulting array and return it
+            int *res = malloc(sizeof(int) * 2);
+            res[0] = item->value;
+            res[1] = i;
+            *returnSize = 2;
+
+            // Iter allows us safely delete item and free it's memory from heap
+            HASH_ITER(hh, hashTable, item, tmpItem) {
+                HASH_DEL(hashTable, item);
+                free(item);
+            }
+            return res;
+        }
+        // Fill up hash map if match was not found
+        item = malloc(sizeof(struct hashTable));
+        item->key = target - nums[i];
+        item->value = i;
+        HASH_ADD_INT(hashTable, key, item);
+    }
+
+    // No match was found, delete item and free it's memory from heap
+    HASH_ITER(hh, hashTable, item, tmpItem) {
+        HASH_DEL(hashTable, item);
+        free(item);
+    }
+
+    // Return size is 0, returning valid pointer
+    *returnSize = 0;
+    return malloc(sizeof(int) * 0);
+}
+#endif // SOLUTION_HASH_TABLE
+
+// O(n^2) - time, O(n) - memory
+#if SOLUTION == SOLUTION_BRUTE_FORCE
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     int *solution = (int *)malloc(2 * sizeof(int));
@@ -16,3 +70,4 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
     
     return 0;
 }
+#endif // SOLUTION_BRUTE_FORCE
