928c3898d69aa5e4a207411924b10236cd6cf607
AleksSH97 <alexnv97@gmail.com>
2026-07-18 22:57:13 +0300

    [+] update Valid Parentheses solution (Stack, O(N) space and time)

--- c/easy/20_valid_parentheses.c
+++ c/easy/20_valid_parentheses.c
@@ -1,113 +1,87 @@
-typedef struct stack {
-    int max;
-    int top;
-    char *item;
-} stack_t;
+#include <stdbool.h>
 
-static stack_t *init_stack(int max);
-static void pop_stack(stack_t *stack);
-static void push_stack(char s, stack_t *stack);
-static bool is_full_stack(stack_t *stack);
-static bool is_empty_stack (stack_t *stack);
-static void destroy_stack(stack_t *stack);
+#define PARENTHESIS_OPEN  40 // ()
+#define PARENTHESIS_CLOSE 41
 
-stack_t *init_stack(int max)
-{
-    stack_t *stack = (stack_t *)malloc(sizeof(struct stack));
-    stack->top = -1;
-    stack->max = max;
-    stack->item = (char *)malloc(max * sizeof(char));
+#define SQUARE_BRACKET_OPEN  91 // []
+#define SQUARE_BRACKET_CLOSE 93
 
-    return stack;
+#define BRACES_OPEN  123 // {}
+#define BRACES_CLOSE 125
+
+typedef enum {
+    NOT_BRACKET,
+
+    PARENTHESIS,
+    SQUARE_BRACKET,
+    BRACES,
+} brackets_type_t;
+
+bool is_opening(int symbol) {
+    return ((symbol == PARENTHESIS_OPEN) || 
+           (symbol == SQUARE_BRACKET_OPEN) || 
+           (symbol == BRACES_OPEN)) ? true : false;
 }
 
-void destroy_stack(stack_t *stack)
-{
-    free(stack->item);
-    free(stack);
-    stack = NULL;
+bool is_closing(int symbol) {
+    return ((symbol == PARENTHESIS_CLOSE) || 
+           (symbol == SQUARE_BRACKET_CLOSE) || 
+           (symbol == BRACES_CLOSE)) ? true : false;
 }
 
-void pop_stack(stack_t *stack) {
-
-    if (is_empty_stack(stack)) {
-        printf("Stack empty!\n");
-        abort();
-    }
-   
-    // strcpy(data, stack->item[stack->top]);
-    stack->top--;
-}
-
-void push_stack(char s, stack_t *stack)
-{
-    if (is_full_stack(stack)) {
-        printf("Stack if full!\n");
-        abort();
+brackets_type_t get_brackets_type(int symbol) {
+    switch(symbol) {
+        case PARENTHESIS_OPEN:
+        case PARENTHESIS_CLOSE:
+            return PARENTHESIS;
+        case SQUARE_BRACKET_OPEN:
+        case SQUARE_BRACKET_CLOSE:
+            return SQUARE_BRACKET;
+        case BRACES_OPEN:
+        case BRACES_CLOSE:
+            return BRACES;
     }
 
-    stack->top++;
-    stack->item[stack->top] = s;
+    return NOT_BRACKET;
 }
 
-bool is_empty_stack (stack_t *stack)
-{
-    return (stack->top == -1);
-}
+bool isValid(char* s) {
+    int len = strlen(s);
 
-bool is_full_stack(stack_t *stack)
-{
-    return (stack->top == (stack->max - 1));
-}
+    if (len == 1) {
+        return false;
+    }
 
-bool isValid(char *s)
-{
-    bool result = true;
-    int lenght = strlen(s);
-    // char data[80];
+    char *stack = malloc(sizeof(char) * len);
+    int stack_ptr = -1;
+    bool valid = true;
 
-    stack_t *stack = init_stack(lenght);
+    for (int i = 0; i < len; i++) {
+        if (is_opening((int)s[i])) {
+            stack[++stack_ptr] = s[i];
+        } else if (is_closing((int)s[i])) {
+            // If we got closing bracket and nothing is on stack
+            if(stack_ptr < 0) {
+                free(stack);
+                return false;
+            }
 
-    if (s[0] == ')') return false;
-    if (s[0] == ']') return false;
-    if (s[0] == '}') return false;
-    if (strlen(s) == 1) return false;
-
-    for (int i = 0; i < lenght; i++) {
-        switch (s[i]) {
-            case '(':
-                push_stack(s[i], stack);
-                break; 
-            case '[':
-                push_stack(s[i], stack);
-                break;
-            case '{':
-                push_stack(s[i], stack);
-                break;
-
-            case ')':
-                if (is_empty_stack(stack) || stack->item[stack->top] != '(') {
-                    destroy_stack(stack);
-                    return false;
-                }
-                pop_stack(stack);
-                break;
-            case ']':
-                if (is_empty_stack(stack) || stack->item[stack->top] != '[') {
-                    destroy_stack(stack);
-                    return false;
-                }
-                pop_stack(stack);
-                break;
-            case '}':
-                if (is_empty_stack(stack) || stack->item[stack->top] != '{') {
-                    destroy_stack(stack);
-                    return false;
-                }
-                pop_stack(stack);
-                break;
+            // Check brackets type match. If doesn't match
+            if (get_brackets_type((int)stack[stack_ptr]) != get_brackets_type((int)s[i])) {
+                free(stack);
+                return false;
+            } else {
+                // Decrease stack pointer (pop the item)
+                stack_ptr--;
+            }
         }
     }
-    
-    return is_empty_stack(stack);
+
+    // Items are left on the stack that means there were not enough closing brackets
+    if (stack_ptr != -1) {
+        valid = false;
+    }
+
+    free(stack);
+    return valid;
 }
