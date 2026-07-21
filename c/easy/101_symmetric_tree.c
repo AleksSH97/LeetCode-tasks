e55ebdfb127ba156239892dfd78a4348713e6d05
AleksSH97 <alexnv97@gmail.com>
2026-07-21 22:29:36 +0300

    [+] update Symmetric Tree solution (O(H) space and O(N) time)

--- c/easy/101_symmetric_tree.c
+++ c/easy/101_symmetric_tree.c
@@ -1,49 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-bool traversal(struct TreeNode* left, struct TreeNode* right)
-{
-    if ((left == NULL) && (right == NULL)) {
+#include <stdbool.h>
+#include <stddef.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool check_if_symmetric(struct TreeNode *left, struct TreeNode *right) {
+    if (left == NULL && right == NULL) {
         return true;
-    }
-
-    if ((left == NULL) && (right != NULL)) {
+    } else if (left == NULL || right == NULL) { 
         return false;
     }
 
-    if ((left != NULL) && (right == NULL)) {
+    if (left->val != right->val) {
         return false;
     }
 
-    if (left->val == right->val) {
-        if (!traversal(left->left, right->right)) {
-            return false;
-        }
-        if (!traversal(left->right, right->left)) {
-            return false;
-        }
-    }
-    else {
+    bool symmetric = check_if_symmetric(left->right, right->left);
+
+    if (symmetric == false) {
         return false;
     }
 
-    return true;
+    symmetric = check_if_symmetric(left->left, right->right);
+
+    return symmetric;
 }
 
-bool isSymmetric(struct TreeNode* root)
-{
+bool isSymmetric(struct TreeNode* root) {
     if (root == NULL) {
         return true;
     }
-    
-    if (!traversal(root->left, root->right)) {
-        return false;
-    }
 
-    return true;
+    return check_if_symmetric(root->left, root->right);
 }
